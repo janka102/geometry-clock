fad5c41a4ea89d6fb39b93dd6c056468912d477a
Jesse Smick <janka102@gmail.com>
2014-05-22 13:00:45 -0700

    Added a way to change the favicon to the current time

--- public/js/clock.js
+++ public/js/clock.js
@@ -4,9 +4,60 @@ var clockCenter,
     clockHour,
     clockMinute,
     clockSecond,
-    digitalClock;
+    digitalClock,
+    faviconCounter = 0;
+
+function changeFavicon(src) {
+    var link = document.createElement('link'),
+        oldLink = document.getElementById('favicon');
+
+    link.id = 'favicon';
+    link.rel = 'shortcut icon';
+    link.href = src;
+
+    if (oldLink) {
+        document.head.removeChild(oldLink);
+    }
+
+     document.head.appendChild(link);
+}
+
+function canvasToFavicon() {
+    var canvas = document.getElementById('resizer'),
+        ctx = canvas.getContext('2d'),
+        width = 16,
+        height = 16;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.fillStyle = '#111111';
+    ctx.beginPath();
+    ctx.arc(width / 2, height / 2, 8, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.strokeStyle = 'rgb(255,255,0)';
+    ctx.fillStyle = 'rgb(255,255,0)';
+    ctx.beginPath();
+    ctx.moveTo(width / 2, height / 2);
+    ctx.lineTo(clockHour.x * (16/600), clockHour.y * (16/600));
+    ctx.lineTo(clockMinute.x * (16/600), clockMinute.y * (16/600));
+    ctx.closePath();
+    ctx.lineJoin = 'round';
+    ctx.lineWidth = 1;
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+
+    changeFavicon(canvas.toDataURL());
+}
 
 function create() {
+    game.stage.disableVisibilityChange = false;
+    
     clockCenter = new Phaser.Point(game.world.centerX, game.world.centerY);
     clockHour = new Phaser.Point(clockCenter.x, clockCenter.y - 175);
     clockMinute = new Phaser.Point(clockCenter.x, clockCenter.y - 250);
@@ -22,6 +73,12 @@ function create() {
 }
 
 function update() {
+    if (faviconCounter % 30 === 0) {
+        canvasToFavicon(game.canvas.toDataURL());
+        faviconCounter = 0;
+    }
+    faviconCounter++;
+
     // I subtract 90deg because clocks start at -90deg
     var now = new Date(),
         millisecond = (now.getMilliseconds() * 0.36) - 90,
@@ -41,6 +98,7 @@ function render() {
     game.context.beginPath();
     game.context.arc(clockCenter.x, clockCenter.y, 290, 0, 2 * Math.PI, false);
     game.context.fill();
+    game.context.closePath();
 
     game.context.strokeStyle = 'rgb(255,255,0)';
     game.context.fillStyle = 'rgb(255,255,0)';
